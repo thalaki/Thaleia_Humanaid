94d6b03a1fa43f754282ec3261c1f6131b324c1f
thalaki <temmanouilidi@gmail.com>
2023-06-08 19:29:03 +0300

    Λειτουργεί το μενού

--- app/src/main/java/com/example/humanaid/MainActivity.java
+++ app/src/main/java/com/example/humanaid/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.humanaid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.humanaid.databinding.ActivityMainBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_home);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.service:
+                    replaceFragment(new ServicesFragment());
+                    break;
+                case R.id.physio:
+                    replaceFragment(new PhysioFragment());
+                    break;
+
+            }
+
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment (Fragment fragment){
+        FragmentManager  fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
